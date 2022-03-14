619bc027f00e90fedaaea4a384ed8966a48e8ea9
IbragimDodov <utisey@gmail.com>
2022-03-14 19:35:26 +0300

    #4 complited

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -25,33 +25,6 @@ function Categories ({items}) {
       </ul>
     </div>
   )
-  // state = {
-  //   activeItem: 3
-  // };
-
-  // onSelectItem = (index) => {
-  //   this.setState({
-  //     activeItem: index,
-  //   });
-  // };
-
-  // render() {
-  //   const { items, onClickItem } = this.props;
-
-  //   return (
-  //     <div className="categories">
-  //       <ul>
-  //         <li className="active">Все</li>
-  //         {items.map((name, index) => (
-  //             <li
-  //               className={this.state.activeItem === index ? 'active' : ''}
-  //               onClick={() => this.onSelectItem(index)}
-  //               key={`${name}_${index}`} >{name}</li>
-  //           ))}
-  //       </ul>
-  //     </div>
-  //   )
-  // };
 };
 
 
